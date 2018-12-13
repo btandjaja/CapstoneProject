f4c14a7f87d36dbd97affd88374bb91e2809410a
buntoro tandjaja <wen_college@yahoo.com>
2018-12-13 11:01:13 -0800

    removed extra code

--- app/src/main/java/com/buntorotandjaja/www/capstoneproject/ThreeTwoImageView.java
+++ app/src/main/java/com/buntorotandjaja/www/capstoneproject/ThreeTwoImageView.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-package com.buntorotandjaja.www.capstoneproject;
-
-import android.content.Context;
-import android.support.v7.widget.AppCompatImageView;
-import android.util.AttributeSet;
-
-public class ThreeTwoImageView extends AppCompatImageView {
-    public ThreeTwoImageView(Context context) {
-        super(context);
-    }
-
-    public ThreeTwoImageView(Context context, AttributeSet attrSet) {
-        super(context, attrSet);
-    }
-
-    public ThreeTwoImageView(Context context, AttributeSet attrSet, int defStyle) {
-        super(context, attrSet, defStyle);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * 2 / 3;
-        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
-        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
-    }
-}
-||||||| merged common ancestors
-=======
 package com.buntorotandjaja.www.capstoneproject;
 
 import android.content.Context;
@@ -61,4 +32,3 @@ android:layout_width="match_parent"
 android:layout_height="0dp"
 android:scaleType="ceterCrop"
  */
->>>>>>> access_firebase
