48e62c4b0c56acedb23b46f2381c264e584b5104
buntoro tandjaja <wen_college@yahoo.com>
2018-12-30 15:54:34 -0800

    camera not working

--- app/src/main/java/com/buntorotandjaja/www/capstoneproject/SellActivity.java
+++ app/src/main/java/com/buntorotandjaja/www/capstoneproject/SellActivity.java
@@ -1,7 +1,11 @@
 package com.buntorotandjaja.www.capstoneproject;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,10 +19,13 @@ import com.squareup.picasso.Picasso;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,10 +34,17 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SellActivity extends AppCompatActivity {
 
     private final static int PICK_IMAGE_REQUEST = 1;
     private final static int REQUEST_IMAGE_CAPTURE = 2;
+    private final static int PERMISSION_REQUEST_CODE = 3;
+    private final static String TAG = SellActivity.class.getSimpleName();
 
     @BindView(R.id.toolbar_sell_acitivty) Toolbar mToolbar;
     @BindView(R.id.imageButton_take_picture) ImageButton mTakePicture;
@@ -40,6 +54,7 @@ public class SellActivity extends AppCompatActivity {
 
     private String mUId;
     private Uri mImageUri;
+    private String mCurrentPhotoPath;
 
     // Firebase
     private FirebaseFirestore mDb;
@@ -52,6 +67,12 @@ public class SellActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sell);
         ButterKnife.bind(this);
         setupToolbar();
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST_CODE);
+        }
 
 
         // TODO needed when
@@ -79,12 +100,14 @@ public class SellActivity extends AppCompatActivity {
         mTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                } else {
-                    Toast.makeText(SellActivity.this, getString(R.string.no_camera_available), Toast.LENGTH_SHORT).show();
-                }
+                dispatchTakePictureIntent();
+//                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//                    File photoFile = createImageFile();
+//                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//                } else {
+//                    Toast.makeText(SellActivity.this, getString(R.string.no_camera_available), Toast.LENGTH_SHORT).show();
+//                }
             }
         });
     }
@@ -103,16 +126,103 @@ public class SellActivity extends AppCompatActivity {
         startActivityForResult(imageFromFileIntent, PICK_IMAGE_REQUEST);
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                // Error occurs while creating file
+                Log.e(TAG, e.toString());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this,
+                        "com.buntorotandjaja.www.capstoneproject.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = null;
+//        try {
+//            image = File.createTempFile(imageFileName, ".jpg", storageDir);
+//        } catch (IOException e) {
+//            Log.e(TAG, e.toString());
+//        }
+//        mCurrentPhotoPath = image.getAbsolutePath();
+//        return image;
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,     /* prefix */
+                ".jpg",     /* suffix */
+                storageDir         /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if ((requestCode == PICK_IMAGE_REQUEST || requestCode == REQUEST_IMAGE_CAPTURE) &&
-                resultCode == RESULT_OK && data != null && data.getData() != null) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null &&
+                data.getData() != null) {
             mImageUri = data.getData();
             Picasso.get().load(mImageUri).fit().centerCrop().into(mItemImage);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null &&
+                data.getData() != null) {
+            galleryAddPic();
+            setPic();
         } else {
-            // TODO show error?
+            Toast.makeText(this, "An error occurs.", Toast.LENGTH_LONG).show();
+            return;
         }
+    }
+
+    // save picture
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get imageView dimension
+        int targetW = mItemImage.getWidth();
+        int targetH = mItemImage.getHeight();
+
+        // Get dimension of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mItemImage.setImageBitmap(bitmap);
     }
 
     @Override
